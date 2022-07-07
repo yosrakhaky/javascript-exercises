e48aa52862ce4691efc7f9d8108a1974eb818286
yosrakhaky <yosrakhaky@gmail.com>
2022-07-07 00:06:53 -0400

    add a loop that repeats the string a number of times

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+let finalString = ""
+const repeatString = function(string,num) {
+    for (let i=0; i<num; i++) {
+        finalString += string
+    }
+    return finalString
 };
 
 // Do not edit below this line
