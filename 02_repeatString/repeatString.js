176be9cfde713e209ddc96c61e7653cc85f66f67
yosrakhaky <yosrakhaky@gmail.com>
2022-07-07 00:16:07 -0400

    catch errors using if/else statements

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,9 +1,14 @@
 const repeatString = function(string,num) {
     var finalString = ""
-    for (let i=0; i<num; i++) {
-        finalString += string
+    if (num >= 0) {
+        for (let i=0; i<num; i++) {
+            finalString += string;
+        }
+        return finalString
+    } else {
+        return "ERROR"
     }
-    return finalString
+
 };
 
 // Do not edit below this line
