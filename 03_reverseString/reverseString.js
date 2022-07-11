302592d40f8e0ba3a622aa7314c2a0dd1c208392
yosrakhaky <yosrakhaky@gmail.com>
2022-07-11 15:02:40 -0400

    introduce variables to the function, and allow infinite arguments using the argument object

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    var newString = ""
+    var reversedString = []
+    splitString = string.split("");
+    for (i=0; i<string.length; i++) {
+        reversedString += splitString.pop();
+    }
+    newString = reversedString.toString()
+    return newString
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,6 @@
 const removeFromArray = function() {
-
+    var initArray = arguments[0];
+    var finalArray = [];
 };
 
 // Do not edit below this line
