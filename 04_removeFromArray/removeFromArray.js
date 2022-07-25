66468f0a87876d4a6a6ed3fad68bdab34cc7780c
yosrakhaky <yosrakhaky@gmail.com>
2022-07-25 14:21:56 -0400

    converted function arguments into an array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,24 @@
 const removeFromArray = function() {
-    var initArray = arguments[0];
+    const args = Array.from(arguments);
+    var initArray = args[0];
+    console.log(initArray);
+    console.log(args);
+    var removeArray = [];
     var finalArray = [];
+    for (i=1; i<arguments.length; i++) {
+        indexToRemove = initArray.indexOf(arguments[i+1]); // find index of value inside argument to be removed from array
+        removeArray.push(indexToRemove); //collect indices of values to be removed into one array
+     }
+    console.log(removeArray);
+
+    for (i=0; i<removeArray.length; i++) {
+        finalArray = initArray.splice(removeArray[i]);
+    }
+    
+
+    console.log(finalArray);
+    console.log(initArray);
+
 };
 
 // Do not edit below this line
