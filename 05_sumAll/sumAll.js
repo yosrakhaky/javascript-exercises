938bde093b802b72039e796e9f25d859981f3c34
yosrakhaky <yosrakhaky@gmail.com>
2022-07-25 20:36:47 -0400

    create function that checks that the two entered arguments are positive integers, then decides the min and max bounds, then creates a loop that sums every number in between the 2 positive integers

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function(firstNum, lastNum) {
+    var sum = 0
+    if (Number.isInteger(firstNum) && Number.isInteger(lastNum) && firstNum>=0 && lastNum >=0) {
+        if (firstNum <= lastNum) {
+            var min = firstNum;
+            var max = lastNum;
+        }
+        else if (lastNum<firstNum){
+            var min = lastNum;
+            var max = firstNum;
+        }
+        for (i=min-1; i<max+1; i++) {
+            sum += i
+        }
+    return sum
+    }
+    else {
+        return 'ERROR'
+    }
 };
 
 // Do not edit below this line
