95b0709fd8b9badd76732125b37193c21598ed64
yosrakhaky <yosrakhaky@gmail.com>
2022-08-30 11:54:23 -0400

    create a function that takes an array as argument, then sums up each number in the array and returns the sum

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -10,8 +10,12 @@ const subtract = function(num1, num2) {
 	return num1-num2
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	var sum = 0;
+  for (var i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum;
 };
 
 const multiply = function() {
