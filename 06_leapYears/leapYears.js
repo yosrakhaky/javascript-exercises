0cd7a52559d62706ca7d97f2c6554f586a28718e
yosrakhaky <yosrakhaky@gmail.com>
2022-08-30 11:20:23 -0400

    create a function that recognises leap years and returns true/false to years inputted

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,17 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    leapY=year/4
+    console.log(leapY)
+    console.log(year/100)
 
+    if ((Math.round(year/100) == year/100) && (Math.round(year/400) != year/400)) {
+        return false
+    }
+    else if (Math.round(leapY) ==leapY) {
+        return true
+    }
+    else {
+        return false
+    }
 };
 
 // Do not edit below this line
